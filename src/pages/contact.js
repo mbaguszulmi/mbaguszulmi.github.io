a5cd68e7c2844de2ddc43ec2ba68e9b9ca754455
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-02-22 18:23:00 +0700

    automate email send when submitting form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,23 @@ import React, { Component } from "react"
 class Contact extends Component {
     componentDidMount() {
         document.title = "Contact | Muhammad Bagus Zulmi"
+
+        let contactForm = document.querySelector("#email-form")
+        let subjectInput = document.querySelector("#subject")
+        let bodyInput = document.querySelector("#body")
+        contactForm.addEventListener("submit", event => {
+            let subjectValue = subjectInput.value
+            let bodyValue = bodyInput.value
+
+            if (subjectValue.trim().length !== 0 && bodyValue.trim().length !== 0) {
+                event.preventDefault()
+                let urlToOpen = `mailto:mbaguszulmi@gmail.com?subject=${subjectValue}&body=${bodyValue}`
+                let anchorLink = document.createElement("a")
+                anchorLink.href = urlToOpen
+                anchorLink.target = "_blank"
+                anchorLink.click()
+            }
+        })
     }
 
     render() {
@@ -16,12 +33,12 @@ class Contact extends Component {
 
                     <form className="nx-form" id="email-form">
                         <div className="input-wrapper">
-                            <label for="">Subject</label>
+                            <label htmlFor="subject">Subject</label>
                             <input type="text" name="subject" id="subject" placeholder="Type a subject..." required></input>
                         </div>
 
                         <div className="input-wrapper">
-                            <label for="">Body</label>
+                            <label htmlFor="body">Body</label>
                             <textarea name="body" id="body" placeholder="Body of email" row="4" required></textarea>
                         </div>
 
