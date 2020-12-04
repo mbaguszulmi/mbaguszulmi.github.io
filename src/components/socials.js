787eb3a4b16b7d449cb9be31cc04cef8e0f0b388
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-12-04 15:03:39 +0700

    feat(socials): add youtube icon to socials components

--- src/components/socials.js
+++ src/components/socials.js
@@ -8,6 +8,7 @@ const Socials = () => {
             <a rel="noopener noreferrer" href="https://twitter.com/zulmist_" target="_blank" className="twitter"> </a>
             <a rel="noopener noreferrer" href="https://www.instagram.com/zulmist_" target="_blank" className="instagram"> </a>
             <a rel="noopener noreferrer" href="https://github.com/mbaguszulmi" target="_blank" className="github"> </a>
+            <a rel="noopener noreferrer" href="https://www.youtube.com/channel/UCmJHdPPDq2EnblWmkxdzhmw" target="_blank" className="youtube"> </a>
         </div>
     )
 }
