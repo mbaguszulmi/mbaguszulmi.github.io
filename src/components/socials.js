93857be0ce373a28ea3155d956384914c28c8698
mbaguszulmi <baguszulmi101198@gmail.com>
2021-12-23 00:33:00 +0700

    fix(socials): update social links

--- src/components/socials.js
+++ src/components/socials.js
@@ -3,12 +3,12 @@ import React from "react"
 const Socials = () => {
     return (
         <div className="socials">
-            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/muhammad-bagus-z-724158122/" target="_blank" className="linkedin"> </a>
+            <a rel="noopener noreferrer" href="https://www.linkedin.com/in/mbaguszulmi" target="_blank" className="linkedin"> </a>
             <a rel="noopener noreferrer" href="https://www.facebook.com/M.Bagus.Zulmi" target="_blank" className="facebook"> </a>
             <a rel="noopener noreferrer" href="https://twitter.com/zulmist_" target="_blank" className="twitter"> </a>
             <a rel="noopener noreferrer" href="https://www.instagram.com/zulmist_" target="_blank" className="instagram"> </a>
             <a rel="noopener noreferrer" href="https://github.com/mbaguszulmi" target="_blank" className="github"> </a>
-            <a rel="noopener noreferrer" href="https://www.youtube.com/channel/UCmJHdPPDq2EnblWmkxdzhmw" target="_blank" className="youtube"> </a>
+            <a rel="noopener noreferrer" href="https://www.youtube.com/c/MuhammadBagusZulmi" target="_blank" className="youtube"> </a>
         </div>
     )
 }
