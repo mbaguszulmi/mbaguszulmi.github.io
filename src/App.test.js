78dd672ebe8b0b9fd36ae9783752162270bc8964
mbaguszulmi <baguszulmi101198@gmail.com>
2021-12-24 20:07:43 +0700

    feat(test): add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
-test('Test Open', () => {
-  expect(true).toBeTruthy()
+import { render, screen } from '@testing-library/react';
+import { appRenderer } from './appRenderer';
+
+test('Test Landing Page', () => {
+  render(appRenderer());
+
+  const myName = screen.getAllByText('Muhammad Bagus Zulmi');
+  myName.forEach(name => {
+    expect(name).toBeInTheDocument();
+  })
 })
